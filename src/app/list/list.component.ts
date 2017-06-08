fb386f180413bdb46536d5bdd7c5f47f923a7201
m3tiii <mateuszglowas@gmail.com>
2017-06-08 12:10:41 +0200

    Setup list component.

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  private elements: any[];
+
+  constructor() {
+  	this.elements = ['test1'];
+  }
 
   ngOnInit() {
   }
