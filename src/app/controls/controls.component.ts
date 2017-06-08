e5c4e502f7bd7299a7c97042407f00bc61e5ef23
Radoslaw Wojtysiak <worad94@gmail.com>
2017-06-08 12:46:17 +0200

    Adding base link rege

--- src/app/controls/controls.component.ts
+++ src/app/controls/controls.component.ts
@@ -1,19 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+const baseLinkPattern:RegExp = /https:\/\/steamcommunity.com\/tradeoffer\/new\/\?/;
+
 @Component({
   selector: 'app-controls',
   templateUrl: './controls.component.html',
   styleUrls: ['./controls.component.scss']
 })
+
+
 export class ControlsComponent implements OnInit {
+  
   constructor() { }
 
   ngOnInit() {
 
   }
 
+  showErrorMessage(text):String{
+    console.log(text);
+    return text;
+  }
+
   getInputValue(val) {
-    console.log(val);    
+    let link: string = val;
+    let compareBase: boolean = baseLinkPattern.test(link);
+    
+    if(compareBase){
+
+    }else{
+      this.showErrorMessage('Your link is not from SteamCommunity')
+    }
+    
   }
 
 
